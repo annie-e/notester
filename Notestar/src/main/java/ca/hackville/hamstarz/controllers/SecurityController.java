3404819e3637c1a46cacc7530550af41c039d133
Jenny Juyoung Jung <jungjuyo@sheridancollege.ca>
2022-01-15 01:19:26 -0500

    add spring security

---
+++ Notestar/src/main/java/ca/hackville/hamstarz/controllers/SecurityController.java
@@ -0,0 +1,19 @@
+package ca.hackville.hamstarz.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+	
+	@GetMapping("/login")
+	public String goLogin() {
+		return "login.html";
+	}
+	
+	@GetMapping("/access-denied")
+	public String accessdenied() {
+		return "access-denied.html";
+	}
+
+}
